66d2e7c38696d00c63baee7af5faa08b335861c7
Aaron Daniel <aaronmathewdaniel@outlook.com>
2022-02-27 08:46:20 +0530

    more.css file

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-(function(){
-  const cartbtn = document.querySelectorAll('.cart-add')
-
-  cartbtn.forEach(function(btn){
-    btn.addEventListener('click',function(event){
-      console.log(event.target)
-    })
-  })
-}
-)
+ma
